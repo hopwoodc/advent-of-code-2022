d36b78534d3cc826113450ded286c089a63763e6
Chris Hopwood <chris@hopwood.io>
2022-12-05 19:09:48 -0600

    day 1 part 1

---
+++ day1/part1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace day1
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //For each elf, track how many calories they hold so far
+                int currentCalories = 0;
+                //Track the elf with the most calories here
+                int mostCalories = 0;
+                foreach (string line in File.ReadLines(@"./input.txt")){
+                    var trimmed = line.Trim();
+                    //Elves are separated by empty lines
+                    if (trimmed.Length == 0){
+                        if (mostCalories < currentCalories){
+                            mostCalories = currentCalories;
+                        }
+                        currentCalories = 0;
+                        continue;
+                    }
+                    //Add the elve's "snacks" to the total calories
+                    int parsed;
+                    if (int.TryParse(trimmed, out parsed)){
+                        currentCalories += parsed;
+                    }else{
+                        Console.WriteLine($"Error parsing '{line}'");
+                    }
+                }
+                
+                //Check in case the list of calories didn't include a new line at the end.
+                if (currentCalories>mostCalories){
+                    mostCalories = currentCalories;
+                }
+
+                Console.WriteLine($"The answer is {mostCalories}");
+            } catch (Exception e){
+                Console.WriteLine($"Error: {e.Message}");
+            }
+        }
+    }
+}
